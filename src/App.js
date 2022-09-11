3eaf2a5f34678cdc786f175698982f5e778cf65e
fablorv <fablorvdz@gmail.com>
2022-09-11 03:27:29 +0100

    figured out how to update the page hopefully and added new pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import  {ReactComponent as Logo} from "./assets/shared/logo.svg";
+import {useState} from 'react'
 
 function App() {
-	if(true){
+
+	const [data, setData] = useState('')
+	
+		const parentToChild = () => {setData("this is data for the other thing ig " )}
 		 return (
 		    <div className="App">
 			  <div className="header">
@@ -10,19 +14,16 @@ function App() {
 					<Logo />
 				</div>
 				<div className="buttonscontrol">
-					<button className="home">00 HOME </button>
+					<button className="home" onClick={() => parentToChild()}>00 HOME </button>
 					<button className="destination">01 DESTINATION </button>
 					<button className="crew">02 CREW  </button>
 					<button className="technology">03 TECHNOLOGY </button>
 				</div>
 			  </div>
-			 <Condition/>
+			 <Condition parentToChild={data}/>
 		     </div>
 		  );
 
-	}else{
-		return <div> testing those </div>
-	}
 
 	
  }
@@ -55,8 +56,7 @@ const Testing = () =>{
 	)
 }
 
-const Home = () =>{
-	console.log("from home")
+const Home = () =>{ console.log("from home")
 	return(
 		 <div className="textandexplore">
 			<div className="text">
@@ -71,9 +71,62 @@ const Home = () =>{
 	)
 }
 
-const Condition = () =>{
+const Destination = () =>{
+	console.log("from desitionation")
+	return(
+		<div className="crewd">
+			<p> 02 Meet your crew Commander </p>
+			<div className="crewstuff">
+				<div className="crewparag">
+					<p>COMMANDER </p>
+					<p>DOUGLAS HURLEY </p>
+					<p> Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+				</div>
+				<p> here should be a pic </p>
+
+			</div>
+			<button className="first"/>
+			<button className="second"/>
+			<button className="third"/>
+			<button className="fourth"/>
+		</div>
+	)
+}
+const Technology = () =>{
+	console.log("from destination")
+	return(
+		<div className="technologyD">
+			<p>   Space launch 101</p>
+			<div className="techbottom">
+				<div className="techbuttons">
+					<button className="circleone"> 1 </button>
+					<button className="circletwo"> 2 </button>
+					<button className="circlethree"> 3 </button>
+				</div>
+				<div className="techparag">
+					<p>THE TERMINOLOGY...</p>
+					<p> LAUNCH VEHICLE</p>
+					<p>  A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+				</div>
+				<p> and simply a pic here </p>
+			</div>
+		</div>
+	)
+}
+const Condition = ({parentToChild}) =>{
 	if (false){
-		return 	<Home/>
-	}else {return <Testing/>}
+		return(
+			<div>
+				{parentToChild}	
+				<Home/>
+			</div>
+		)
+	}else if (false){
+		return <Testing/>
+	}else if (false){
+		return <Destination />
+	}else if (true){
+		return <Technology/>
+	}
 }
 export default App;
